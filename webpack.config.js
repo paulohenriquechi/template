ff98883766fbffe9439e7f1b90c08c83cf056f98
Paulo Henrique Chi <paulo.henrique.chi@outlook.com>
2023-02-14 16:22:47 -0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path")
+
+module.exports = {
+    entry: {
+        index: './src/index.js',
+    },
+
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: ['babel-loader']
+            }
+        ]
+    },
+    mode: 'development',
+    devServer: {
+        static: { 
+            directory: path.join(__dirname, './dist') 
+        },
+        liveReload: true
+    }
+}
